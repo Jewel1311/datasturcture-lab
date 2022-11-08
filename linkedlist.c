c2375a4928f2c9f572f585329deeeedb50b13ecf
Jewel <84492029+Jewel1311@users.noreply.github.com>
2022-11-08 21:23:06 +0530

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -4,6 +4,10 @@
 void insbeg();
 void inspos();
 void insend();
+void delbeg();
+void delend();
+void delpos();
+void search();
 int count();
 
 struct node {
@@ -99,12 +103,86 @@ void inspos(){
     }
     
 }
+void delbeg(){
+    if(head == NULL){
+        printf("Linked List empty");
+    }
+    else{
+        struct node *temp;
+        temp = head;
+        head = head->next;
+        printf("%d is deleted", temp->data);
+    }
+}
+
+void delend(){
+    if(head == NULL){
+        printf("Linked List empty");
+    }
+    else if(head->next == NULL){
+        printf("%d is deleted", head->data);
+        head = NULL;
+    }
+    else{
+        struct node *temp;
+        temp = head;
+        while(temp->next->next != NULL){
+            temp = temp->next;
+        }
+        printf("%d is deleted", temp->next->data);
+        temp->next = NULL;
+    }
+}
+void delpos(){
+    int pos,i;
+    struct node *temp;
+    printf("Enter the position to delete : ");
+    scanf("%d", &pos);
+    if(pos == 1){
+        delbeg();
+    }
+    else if(pos > 1 && pos < count()){
+        temp = head;
+        i = 1;
+        while(i < pos-1){
+            temp = temp->next;
+            i++;
+        }
+        printf("%d is deleted", temp->next->data);
+        temp->next = temp->next->next;
+    }
+    else if(pos == count()){
+        delend();
+    }
+    else{
+        printf("Invalid position");
+    }
+}
+
+void search(){
+    int e, f=0;
+    struct node *temp;
+    printf("Enter the element to be searched : ");
+    scanf("%d", &e);
+    temp = head;
+    while(temp != 0){
+        if(temp->data == e){
+            printf("\nElement found");
+            f = 1;
+            break;
+        }
+        temp = temp->next;
+    }
+    if(f == 0){
+        printf("Element not found");
+    }
+}
 
 void main(){
     int choice;
     while(1){
         printf("\n--------------------------------------------\n");
-        printf("\n1.Insertion beg\n2.Insertion postion\n3.Insertion end\n4.Traversal\n5.Exit\n");
+        printf("\n1.Insertion beg\n2.Insertion postion\n3.Insertion end\n4.Traversal\n5.Deletion beg\n6.Deletion end\n7.Deletion pos\n8.Search\n9.Exit\n");
         printf("Enter your choice : ");
         scanf("%d", &choice);
         switch(choice) {
@@ -116,7 +194,15 @@ void main(){
                     break;
             case 4: traversal();
                     break;
-            case 5: exit(0);
+            case 5: delbeg();
+                    break;
+            case 6: delend();
+                    break;
+            case 7: delpos();
+                    break;
+            case 8: search();
+                    break;
+            case 9: exit(0);
                     break;
             default: 
                     printf("Invalid Choice");
