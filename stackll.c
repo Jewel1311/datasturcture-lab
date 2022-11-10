539778fdeb169d4445bbcbad4764709eeee6ad30
Jewel <84492029+Jewel1311@users.noreply.github.com>
2022-11-10 10:26:35 +0530

    Update stackll.c

--- stackll.c
+++ stackll.c
@@ -6,14 +6,43 @@ struct node {
 	struct node *next;
 };
 
+struct node *temp, *top = NULL;
+
 void push() {
-	
+	int val;
+	struct node *newnode;
+	newnode = (struct node *)malloc(sizeof(struct node));
+	printf("\nEnter the element : ");
+	scanf("%d", &val);
+	newnode->data = val;
+	newnode->next = top;
+	top = newnode;			
 }
+void pop(){
+	if(top == NULL){
+		printf("Underflow");
+	}
+	else{
+		temp = top;
+		top = top->next;
+		printf("%d is deleted", temp->data);
+		free(temp); 
+	}
+}
+
+void traversal(){
+	temp = top;
+	while(temp != NULL){
+		printf("%d\n", temp->data);
+		temp = temp->next; 
+	}
+}
+
 
 void main(){
 	int choice;
 	while(1){
-		printf("\n-----------------------\n"
+		printf("\n-----------------------\n");
 		printf("\n1.Push\n2.Pop\n3.Traversal\n4.Exit");
 		printf("\nEnter your choice : ");
 		scanf("%d",&choice);
